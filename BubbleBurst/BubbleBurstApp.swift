68f25604577783479abdbcf02bd0258647e6b741
Matheus Maxwell Meireles <matheus.maxwell@easypallet.com.br>
2022-09-14 18:37:41 -0300

    Initial Commit

---
+++ BubbleBurst/BubbleBurstApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleBurstApp.swift
+//  BubbleBurst
+//
+//  Created by Matheus Maxwell Meireles on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BubbleBurstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
